b887d4df3d0ebd38984c5f76fd53f148216f31f3
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2026-02-25 18:47:20 -0300

    analysis

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,10 +9,10 @@ const prisma = new PrismaClient()
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
-        GitHub,
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    trustHost: true,
 })
