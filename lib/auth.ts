6c2627cd24de76b50bd5b93c962d6fc89f016db2
Sebastian Conte-Grand <sebacontegrand@gmail.com>
2026-02-25 22:01:29 -0300

    fixxxx

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,14 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import { withAccelerate } from "@prisma/extension-accelerate"
 import type { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GitHubProvider from "next-auth/providers/github"
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma as any),
     providers: [
         GoogleProvider({
             clientId: process.env.AUTH_GOOGLE_ID!,
